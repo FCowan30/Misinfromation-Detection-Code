132b694033564e0986a208dce95bb9bb27503d19
Fcowan30 <FCOWAN300@caledonian.ac.uk>
2026-01-05 15:02:54 +0000

    README updated

--- Backend/Preprocessing.py
+++ Backend/Preprocessing.py
@@ -1,9 +1,10 @@
+# import pandas with error handling.
 try:
     import pandas as pd
 except ImportError as e:
     raise ImportError(
         "Pandas is required to run this script (Preproecessing.py)." \
-        " Please install it using 'pip install pandas'."
+        "Please install it using 'pip install pandas'."
     ) from e 
 
 
@@ -13,8 +14,11 @@ def Load_training_data():
     df2 = pd.read_csv("data/True.csv")
 
     print("Dataset loaded successfuylly.")
+
     print("Shape of Fake news dataset: ", df.shape)
     print("Shape of True news dataset: ", df2.shape)
+
+    return df, df2
 
 # Testing the function directly from file.
 if __name__ == "__main__":
--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 This is a misinformation detection Project for a final year honours project, the goal is to use Machine learning to gain insight into the authentisity of statements and to detect misinformation.
 In order to run this system: 
+
 1. Create a python virutal Enviroment.
 2. Install dependencies: see requirements.txt
 3. run preprocessing or back-end scripts as required. 
+
+Returning users: 
+1. start Python Enviroment ".\.venv\Scripts\Activate.ps1"
