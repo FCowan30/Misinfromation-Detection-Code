d0bde9cb030f9a274eb23ef4d99217bd744a3a80
Fcowan30 <FCOWAN300@caledonian.ac.uk>
2026-01-05 15:27:59 +0000

    Dataset Combined/labelled

--- Backend/Preprocessing.py
+++ Backend/Preprocessing.py
@@ -10,16 +10,52 @@ except ImportError as e:
 
 def Load_training_data():
     # load dataset
-    df = pd.read_csv("data/fake.csv")
-    df2 = pd.read_csv("data/True.csv")
+    # ensure Files exist, are accessible, and not empty.
+    try:
+        df_fake= pd.read_csv("data/fake.csv")
+        df_true = pd.read_csv("data/True.csv")
+    except FileNotFoundError as e:
+        raise FileNotFoundError(
+            "Dataset files not found. Please ensure 'fake.csv' and 'True.csv' are present in the 'data' directory."
+        ) from e 
+    except pd.errors.EmptyDataError as e:
+        raise ValueError(
+            "One of the dataset files is empty. Please check the files in the 'data' directory."
+        ) from e
+    except Exception as e:
+        raise Exception(
+            f"An error occurred while loading the dataset files: {e}."
+        ) from e
+    
+    #ensure Files are not empty
+    if df_fake.empty:
+        raise ValueError("The fake news dataset is empty.")
+    
+    if df_true.empty:
+        raise ValueError("The true news dataset is empty.")
 
     print("Dataset loaded successfuylly.")
 
-    print("Shape of Fake news dataset: ", df.shape)
-    print("Shape of True news dataset: ", df2.shape)
+    return df_fake, df_true
 
-    return df, df2
+def label_combine_data(df_fake, df_true):
+    # Add labels
+    df_fake["label"] = 0
+    df_true["label"] = 1
+
+    # Combine datasets
+    df = pd.concat([df_fake, df_true], ignore_index=True)
+
+    # Shuffle the dataset
+    df = df.sample(frac=1).reset_index(drop=True)
+
+    print("Data preprocessed successfully.")
+    print("Dataset shape:", df.shape)
+    print(df.head(5))
+
+    return df
 
 # Testing the function directly from file.
 if __name__ == "__main__":
-    Load_training_data()
+    df_Fake, df_True = Load_training_data()
+    df = label_combine_data(df_Fake, df_True)
