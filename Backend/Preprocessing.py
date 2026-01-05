e5e11649b80cddc1d80d0e7f2354a91f1c8ba69a
Fcowan30 <FCOWAN300@caledonian.ac.uk>
2026-01-05 14:56:26 +0000

    Error handling for pandas Added

--- Backend/Preprocessing.py
+++ Backend/Preprocessing.py
@@ -1,10 +1,17 @@
-import pandas as pd
+try:
+    import pandas as pd
+except ImportError as e:
+    raise ImportError(
+        "Pandas is required to run this script (Preproecessing.py)." \
+        " Please install it using 'pip install pandas'."
+    ) from e 
+
 
 def Load_training_data():
     # load dataset
     df = pd.read_csv("data/fake.csv")
     df2 = pd.read_csv("data/True.csv")
-    
+
     print("Dataset loaded successfuylly.")
     print("Shape of Fake news dataset: ", df.shape)
     print("Shape of True news dataset: ", df2.shape)
