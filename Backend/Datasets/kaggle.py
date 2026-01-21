448e0741854935424cc39563b5a6b7dd8039fe4a
Fcowan30 <FCOWAN300@caledonian.ac.uk>
2026-01-21 15:00:54 +0000

    Kaggle dataframe paths loaded

--- Backend/Datasets/kaggle.py
+++ Backend/Datasets/kaggle.py
@@ -5,3 +5,15 @@ except ImportError as e:
         "Pandas is required to run this script (Preproecessing.py)." \
         "Please install it using 'pip install pandas'."
 ) from e 
+
+from pathlib import Path
+from Backend.config import DATA_DIR
+
+def load_kaggle() -> pd.DataFrame:
+
+    """
+    loads Kaggle True/Fake news dataset and returns a stardard DataFrame.
+    """
+
+
+    return df
