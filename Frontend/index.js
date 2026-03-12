cf7dfbd1494b0b0ecc917337ac935c5957ace5a6
Fcowan30 <FCOWAN300@caledonian.ac.uk>
2026-03-12 22:10:34 +0000

    navbar finalized

--- Frontend/index.js
+++ Frontend/index.js
@@ -4,21 +4,17 @@ const mustacheExpress = require("mustache-express");
 
 const app = express();
 
-//mustache
 app.engine(
   "mustache",
-  mustacheExpress(
-    path.join(__dirname, "views", "partials")
-  )
+  mustacheExpress(path.join(__dirname, "views", "partials"))
 );
-app.set("view engine","mustache");
+
+app.set("view engine", "mustache");
 app.set("views", path.join(__dirname, "views"));
 
-//middleware
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
-//routes
 app.use("/", require("./routes/index"));
 
 const PORT = 3000;
--- Frontend/routes/index.js
+++ Frontend/routes/index.js
@@ -1,22 +1,36 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const multer = require('multer');
-const path = require('path');
+const multer = require("multer");
+const path = require("path");
 
 const { analyzeWithPython } = require("../controllers/analyzeController");
 
-// save uploads in frontend/public/uploads
+// Save uploads in FrontEnd/public/uploads
 const upload = multer({
-    dest: path.join(__dirname,"..", "public", "uploads"),
-    limits: { fileSize: 8 * 1024 * 1024 }, // 8MB file size limit
+  dest: path.join(__dirname, "..", "public", "uploads"),
+  limits: { fileSize: 8 * 1024 * 1024 } // 8MB
 });
 
-router.get("/", (req,res) => {
-    res.render("home", {
-        form_text: "",
-        form_explain: true,
-        has_result: false
-    });
+router.get("/", (req, res) => {
+  res.render("home", {
+    form_text: "",
+    form_explain: true,
+    has_result: false
+  });
+});
+
+router.get("/home_explain", (req, res) => {
+  res.render("home_explain", {
+    form_text: "",
+    form_explain: true,
+    has_result: false
+  });
+});
+
+router.get("/evaluation", (req, res) => {
+  res.render("evaluation", {
+    has_result: false
+  });
 });
 
 router.post("/analyze", upload.single("image"), analyzeWithPython);
